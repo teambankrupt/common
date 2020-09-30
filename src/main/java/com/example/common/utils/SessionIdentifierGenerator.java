e9b75a2779d5ee31a0484986c510ab5005385731
Sayem Hossain <sayemkcn@gmail.com>
2020-09-30 19:25:09 +0600

    [Update] sessionidentifiergenerator

--- src/main/java/com/example/common/utils/SessionIdentifierGenerator.java
+++ src/main/java/com/example/common/utils/SessionIdentifierGenerator.java
@@ -4,24 +4,17 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 public class SessionIdentifierGenerator {
-    private final SecureRandom random = new SecureRandom();
+    private static final SecureRandom random = new SecureRandom();
 
-    public String nextSessionId() {
+    public static String nextSessionId() {
         return new BigInteger(130, random).toString(32);
     }
 
-    public String nextPassword(){
+    public String nextPassword() {
         return new BigInteger(130, random).toString(32);
-//        SecureRandom random = new SecureRandom();
-//        return "p"+(10000+random.nextInt(9000000));
     }
 
-//     public static int generateOTP() {
-//         SecureRandom random = new SecureRandom();
-//         return random.nextInt(999999);
-//     }
     public static int generateOTP() {
-        SecureRandom random = new SecureRandom();
         return 100000 + random.nextInt(89999);
     }
 
