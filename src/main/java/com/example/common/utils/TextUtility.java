6e5eda56eb2d4fe47c7d4513fcf5dc0339b2cec5
Sayem Hossain <sayemkcn@gmail.com>
2021-12-20 00:35:34 +0600

    [Add] func to test if text contains special character

--- src/main/java/com/example/common/utils/TextUtility.java
+++ src/main/java/com/example/common/utils/TextUtility.java
@@ -1,5 +1,8 @@
 package com.example.common.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TextUtility {
     private TextUtility() {
     }
@@ -7,5 +10,12 @@ public class TextUtility {
     public static String removeSpecialCharacters(String str) {
         if (str == null) return null;
         return str.replaceAll("[^A-Za-z0-9]", "");
+    }
+
+    public static boolean containsSpecialCharacter(String str) {
+        if (str == null) return false;
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(str);
+        return m.find();
     }
 }
