e9004f652ef8e1929d431fac2cb19ef7167137bc
Sayem Hossain <sayemkcn@gmail.com>
2021-12-24 15:52:36 +0600

    [Add] add method to slugify

--- src/main/java/com/example/common/utils/TextUtility.java
+++ src/main/java/com/example/common/utils/TextUtility.java
@@ -1,9 +1,17 @@
 package com.example.common.utils;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.text.Normalizer;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextUtility {
+    private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
+    private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
+    private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
+
     private TextUtility() {
     }
 
@@ -17,5 +25,13 @@ public class TextUtility {
         Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(str);
         return m.find();
+    }
+
+    public static String slugify(@NotNull String input) {
+        String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
+        String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
+        String slug = NONLATIN.matcher(normalized).replaceAll("");
+        slug = EDGESDHASHES.matcher(slug).replaceAll("");
+        return slug.toLowerCase(Locale.ENGLISH);
     }
 }
