84957d0de293b2ddd755bc1446356412eb67c830
Sayem Hossain <sayemkcn@gmail.com>
2022-01-24 05:26:42 +0600

    [Add] storing symmetric key in keystore

--- src/main/java/com/example/common/security/EncryptionUtil.java
+++ src/main/java/com/example/common/security/EncryptionUtil.java
@@ -4,26 +4,45 @@ import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.security.*;
 import java.security.cert.CertificateException;
 import java.util.Locale;
 
-public class EncryptionUtil {
+public class EncryptionUtil implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-    private static final EncryptionUtil INSTANCE = new EncryptionUtil();
     private static final Logger logger = LoggerFactory.getLogger(EncryptionUtil.class);
 
-    public EncryptionUtil getInstance() {
-        return INSTANCE;
+    private static KeyStore keyStore;
+    private static String ksFile;
+    private static String ksPassword;
+
+    private static class EncryptionUtilLoader {
+        private static final EncryptionUtil INSTANCE = new EncryptionUtil();
+    }
+
+    public static EncryptionUtil with(String keyStoreFile, String keyStorePassword) throws KeyStoreException {
+        synchronized (EncryptionUtil.class) {
+            EncryptionUtil.ksFile = keyStoreFile;
+            EncryptionUtil.ksPassword = keyStorePassword;
+            keyStore = getKeyStore(keyStoreFile, keyStorePassword);
+        }
+        return EncryptionUtilLoader.INSTANCE;
+    }
+
+    // Protect from serialization should return only one instance
+    @SuppressWarnings("unused")
+    private EncryptionUtil readResolve() {
+        return EncryptionUtilLoader.INSTANCE;
     }
 
     private EncryptionUtil() {
     }
 
-    public static KeyStore getKeyStore(String file, String password) throws KeyStoreException, CertificateException, IOException, NoSuchAlgorithmException {
+    private static KeyStore getKeyStore(String file, String password) throws KeyStoreException {
         if (file == null) throw new RuntimeException("Must provide keystore file path!");
         if (password == null) throw new RuntimeException("Keystore password can't be null!");
         if (!FilenameUtils.getExtension(file).toUpperCase(Locale.ROOT).equals("PKCS12"))
@@ -34,22 +53,50 @@ public class EncryptionUtil {
 
         File keyStoreFile = new File(file);
         if (!keyStoreFile.exists())
-            EncryptionUtil.createNewKeystore(keyStore, file, pwdArray);
+            writeKeyStore(keyStore, file, pwdArray, false);
 
-        InputStream is = new FileInputStream(file);
-
-        keyStore.load(is, pwdArray);
+        try (InputStream is = new FileInputStream(file)) {
+            keyStore.load(is, pwdArray);
+        } catch (CertificateException | IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
         return keyStore;
     }
 
-    private static void createNewKeystore(KeyStore keyStore, String file, char[] pwdArray) throws CertificateException, IOException, NoSuchAlgorithmException {
-        keyStore.load(null, pwdArray);
+    private static void writeKeyStore(KeyStore keyStore, String file, char[] pwdArray, boolean update) {
         try (FileOutputStream fos = new FileOutputStream(file)) {
+            if (!update)
+                keyStore.load(null, pwdArray);
             keyStore.store(fos, pwdArray);
         } catch (IOException | CertificateException | KeyStoreException | NoSuchAlgorithmException e) {
             logger.debug("Could not create keystore!");
             e.printStackTrace();
         }
     }
+
+    public void storeSymmetricKey(String alias, String key, String password) {
+        if (password == null) throw new RuntimeException("Key password can't be null!");
+
+        byte[] keyBytes = key.getBytes();
+        SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
+        KeyStore.SecretKeyEntry secret
+                = new KeyStore.SecretKeyEntry(secretKey);
+        KeyStore.ProtectionParameter pwd
+                = new KeyStore.PasswordProtection(password.toCharArray());
+        try {
+            logger.info("Storing key entry for alias: " + alias + " | secret: " + secret + " | Passphrase: " + pwd);
+            keyStore.setEntry(alias, secret, pwd);
+            writeKeyStore(keyStore, ksFile, ksPassword.toCharArray(), true);
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String retrieveSymmetricKey(String alias, String password) throws UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException {
+        Key key = keyStore.getKey(alias, password.toCharArray());
+        if (key == null) return null;
+        return new String(key.getEncoded());
+    }
+
 
 }
