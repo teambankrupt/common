087c18bbe5020e78364dff8c5d8efa18e16301f1
Sayem Hossain <mail@sayem.dev>
2023-10-08 16:24:20 +0600

    Optional to Option

--- src/main/kotlin/com/example/common/exceptions/utility.kt
+++ src/main/kotlin/com/example/common/exceptions/utility.kt
@@ -1,7 +1,11 @@
 package com.example.common.exceptions
 
+import arrow.core.Option
+import arrow.core.toOption
 import com.example.common.utils.ExceptionUtil
 import java.util.*
 
 inline fun <reified T> Optional<T>.orDuckIt(id: Long): T =
     this.orElseThrow { ExceptionUtil.notFound(T::class.java.simpleName, id) }
+
+fun <T>Optional<T>.toArrow(): Option<T> = this.orElse(null).toOption()
