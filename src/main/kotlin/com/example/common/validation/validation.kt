d02ab4fae8bfa0a52557bf80193a68ab7f3e74b6
Sayem Hossain <mail@sayem.dev>
2024-01-15 19:29:52 +0600

    Added validation scopes

--- src/main/kotlin/com/example/common/validation/validation.kt
+++ src/main/kotlin/com/example/common/validation/validation.kt
@@ -3,37 +3,46 @@ package com.example.common.validation
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
+import arrow.core.valid
 import com.example.common.exceptions.Err
 
-data class ValidatedObject<T>(val obj: T, val valid: Boolean)
+sealed interface ValidationScope {
+    //	READ, WRITE, SEARCH, MODIFY
+    data object Read : ValidationScope
+    data object Write : ValidationScope
+    data object Search : ValidationScope
+    data object Modify : ValidationScope
+}
 
 interface ValidationV2<T> {
-	fun apply(data: T): Either<Err.ValidationErr, T>
+    fun apply(data: T, scope: ValidationScope): Either<Err.ValidationErr, T>
 }
 
 fun <T> genericValidation(
-	message: String? = null,
-	instruction: String = "",
-	valid: (data: T) -> Boolean
+    message: String? = null,
+    instruction: String = "",
+    scopes: Set<ValidationScope>,
+    valid: (data: T) -> Boolean
 ): ValidationV2<T> =
-	object : ValidationV2<T> {
+    object : ValidationV2<T> {
 
-		override fun apply(data: T): Either<Err.ValidationErr, T> =
-			if (valid(data)) {
-				data.right()
-			} else {
-				Err.ValidationErr
-					.GenericValidationErr(RuntimeException(message ?: "$data is invalid"), instruction)
-					.left()
-			}
+        override fun apply(data: T, scope: ValidationScope): Either<Err.ValidationErr, T> =
+            if ((scope !in scopes) || valid(data)) {
+                data.right()
+            } else {
+                Err.ValidationErr
+                    .GenericValidationErr(RuntimeException(message ?: "$data is invalid"), instruction)
+                    .left()
+            }
 
-	}
-class EmailValidation : ValidationV2<String> {
-	override fun apply(data: String): Either<Err.ValidationErr, String> =
-		if ("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$".toRegex().matches(data))
-			data.right()
-		else Err.ValidationErr.EmailValidationErr(
-			RuntimeException("Invalid email address"),
-			"Please input a valid email address"
-		).left()
+    }
+
+class EmailValidation() : ValidationV2<String> {
+    override fun apply(data: String, scope: ValidationScope): Either<Err.ValidationErr, String> =
+        if ("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$".toRegex().matches(data))
+            data.right()
+        else Err.ValidationErr.EmailValidationErr(
+            RuntimeException("Invalid email address"),
+            "Please input a valid email address"
+        ).left()
 }
