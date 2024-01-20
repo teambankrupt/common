5634803c42933cf42246abe143119a87970a7b53
Sayem Hossain <mail@sayem.dev>
2024-01-20 21:28:40 +0600

    Added utility method

--- src/main/java/com/example/common/utils/SessionIdentifierGenerator.java
+++ src/main/java/com/example/common/utils/SessionIdentifierGenerator.java
@@ -23,5 +23,8 @@ public class SessionIdentifierGenerator {
     public static String alphanumeric(int size) {
         return RandomStringUtils.randomAlphanumeric(size);
     }
+    public static String numeric(int size) {
+        return RandomStringUtils.randomNumeric(size);
+    }
 
 }
