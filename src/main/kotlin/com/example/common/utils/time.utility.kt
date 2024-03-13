e4667928435f2f175654b30a586c75fae3ee42f8
Sayem Hossain <mail@sayem.dev>
2024-03-13 17:35:28 +0600

    Added daystart and end ext for instant

--- src/main/kotlin/com/example/common/utils/time.utility.kt
+++ src/main/kotlin/com/example/common/utils/time.utility.kt
@@ -1,8 +1,10 @@
 package com.example.common.utils
 
 import java.time.Instant
+import java.time.LocalDateTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 fun Instant.toReadableDateTime(
     zoneId: ZoneId
@@ -10,3 +12,12 @@ fun Instant.toReadableDateTime(
     this.atZone(zoneId).format(
         DateTimeFormatter.ofPattern("MMM dd, YYYY hh:mm a")
     )
+
+fun Instant.dayStart(zoneId: ZoneId): Instant =
+    LocalDateTime.ofInstant(this, zoneId).toLocalDate()
+        .atStartOfDay(zoneId).toInstant()
+
+fun Instant.dayEnd(zoneId: ZoneId): Instant =
+    this.dayStart(zoneId)
+        .plus(24, ChronoUnit.HOURS)
+        .minusMillis(1)
