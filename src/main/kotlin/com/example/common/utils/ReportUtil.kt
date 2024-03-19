fcf7f8120b05180d5a5e0cacd6a927eb616f03c3
Sayem Hossain <mail@sayem.dev>
2024-03-19 20:38:38 +0600

    Optimized

--- src/main/kotlin/com/example/common/utils/ReportUtil.kt
+++ src/main/kotlin/com/example/common/utils/ReportUtil.kt
@@ -5,24 +5,20 @@ import java.io.IOException
 import java.util.*
 
 
-class ReportUtil private constructor() {
+object ReportUtil {
 
-    companion object {
+    @Throws(IOException::class, InterruptedException::class)
+    fun generatePdf(url: String): File {
+        val fileName = UUID.randomUUID().toString() + ".pdf"
+        Shell.exec("wkhtmltopdf $url $fileName")
+        return File(fileName)
+    }
 
-        @Throws(IOException::class, InterruptedException::class)
-        fun generatePdf(url: String): File {
-            val fileName = UUID.randomUUID().toString() + ".pdf"
-            Shell.exec("wkhtmltopdf $url $fileName")
-            return File(fileName)
-        }
-
-        @Throws(IOException::class, InterruptedException::class)
-        fun generateImage(url: String): File {
-            val fileName = UUID.randomUUID().toString() + ".png"
-            Shell.exec("wkhtmltoimage $url $fileName")
-            return File(fileName)
-        }
-
+    @Throws(IOException::class, InterruptedException::class)
+    fun generateImage(url: String): File {
+        val fileName = UUID.randomUUID().toString() + ".png"
+        Shell.exec("wkhtmltoimage $url $fileName")
+        return File(fileName)
     }
 
 }
