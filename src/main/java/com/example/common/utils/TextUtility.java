8c58056f5a81252f512f51d70fecaff0a5385d99
Sayem Hossain <mail@sayem.dev>
2024-09-17 20:36:11 +0600

    Added domain validation

--- src/main/java/com/example/common/utils/TextUtility.java
+++ src/main/java/com/example/common/utils/TextUtility.java
@@ -8,30 +8,36 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TextUtility {
-    private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
-    private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
-    private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
+	private static final Pattern NONLATIN = Pattern.compile("[^\\w-]");
+	private static final Pattern WHITESPACE = Pattern.compile("[\\s]");
+	private static final Pattern EDGESDHASHES = Pattern.compile("(^-|-$)");
 
-    private TextUtility() {
-    }
+	private TextUtility() {
+	}
 
-    public static String removeSpecialCharacters(String str) {
-        if (str == null) return null;
-        return str.replaceAll("[^A-Za-z0-9]", "");
-    }
+	public static String removeSpecialCharacters(String str) {
+		if (str == null) return null;
+		return str.replaceAll("[^A-Za-z0-9]", "");
+	}
 
-    public static boolean containsSpecialCharacter(String str) {
-        if (str == null) return false;
-        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
-        Matcher m = p.matcher(str);
-        return m.find();
-    }
+	public static boolean containsSpecialCharacter(String str) {
+		if (str == null) return false;
+		Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(str);
+		return m.find();
+	}
 
-    public static String slugify(@NotNull String input) {
-        String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
-        String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
-        String slug = NONLATIN.matcher(normalized).replaceAll("");
-        slug = EDGESDHASHES.matcher(slug).replaceAll("");
-        return slug.toLowerCase(Locale.ENGLISH);
-    }
+	public static String slugify(@NotNull String input) {
+		String nowhitespace = WHITESPACE.matcher(input).replaceAll("-");
+		String normalized = Normalizer.normalize(nowhitespace, Normalizer.Form.NFD);
+		String slug = NONLATIN.matcher(normalized).replaceAll("");
+		slug = EDGESDHASHES.matcher(slug).replaceAll("");
+		return slug.toLowerCase(Locale.ENGLISH);
+	}
+
+	public static boolean isValidDomain(String domain) {
+		return Pattern
+				.compile("^(?!://)([a-zA-Z0-9-_]+\\.)*[a-zA-Z0-9][a-zA-Z0-9-_]+\\.[a-zA-Z]{2,11}?$")
+				.matcher(domain).matches();
+	}
 }
