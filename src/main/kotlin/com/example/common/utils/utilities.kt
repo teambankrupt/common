f72b4ff7b52c4f29142998915ca710d12b021983
Sayem Hossain <mail@sayem.dev>
2024-09-19 01:44:36 +0600

    Added utility method

--- src/main/kotlin/com/example/common/utils/utilities.kt
+++ src/main/kotlin/com/example/common/utils/utilities.kt
@@ -4,12 +4,15 @@ import org.springframework.util.LinkedMultiValueMap
 import org.springframework.util.MultiValueMap
 
 fun Map<String, Set<String>>.toHeaderMultiValueMap() =
-    mapToMultiValueMap(this.mapValues { it.value.joinToString(",") })
+	mapToMultiValueMap(this.mapValues { it.value.joinToString(",") })
 
 fun mapToMultiValueMap(map: Map<String, String>): MultiValueMap<String, String> {
-    val multiValueMap = LinkedMultiValueMap<String, String>()
-    map.forEach { (key, value) ->
-        multiValueMap.add(key, value)
-    }
-    return multiValueMap
+	val multiValueMap = LinkedMultiValueMap<String, String>()
+	map.forEach { (key, value) ->
+		multiValueMap.add(key, value)
+	}
+	return multiValueMap
 }
+
+inline fun <reified T> T?.requiredNotNull(key: String? = null) =
+	this ?: throw ExceptionUtil.notAcceptable("${key ?: "Item"} can't be null")
